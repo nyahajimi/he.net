036e5d5b44ae3c1142ff7a5892e0aba353f54a5f
nyahajimi <7547386+hajimi@gmail.com>
2025-09-20 00:08:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,24 +13,21 @@ echo "--- IPv6 Random Proxy Entrypoint ---"
 echo "------------------------------------"
 
 # --- Network Configuration ---
-echo "[1/4] Configuring IPv6 address for tunnel interface..."
+echo "[1/5] Configuring IPv6 address for tunnel interface..."
 ip addr add "${HE_CLIENT_IPV6}" dev "${HE_TUNNEL_IF}"
-echo "      Interface '${HE_TUNNEL_IF}' is now configured with ${HE_CLIENT_IPV6}."
 
-# !!! --- THIS IS THE FIX --- !!!
-# When an interface is moved to a new network namespace, it is brought down.
-# We must bring it back up before we can use it for routing.
-echo "[2/4] Bringing tunnel interface up..."
+echo "[2/5] Bringing tunnel interface up..."
 ip link set "${HE_TUNNEL_IF}" up
-echo "      Interface '${HE_TUNNEL_IF}' is now UP."
 
-echo "[3/4] Setting default IPv6 route..."
+echo "[3/5] Setting default IPv6 route..."
 ip route add ::/0 dev "${HE_TUNNEL_IF}"
-echo "      Default IPv6 route now points to '${HE_TUNNEL_IF}'."
 
-echo "[4/4] Applying random source NAT (SNAT) rule..."
-ip6tables -t nat -A POSTROUTING -o "${HE_TUNNEL_IF}" -j SNAT --to-source "${HE_ROUTED_PREFIX}" --random
-echo "      iptables rule installed to randomize outgoing IPs from prefix ${HE_ROUTED_PREFIX}."
+echo "[4/5] Applying random source NAT (SNAT) rule..."
+# Extract the network part from the prefix (e.g., 2001:470:c:def::)
+NETWORK_PART=$(echo "${HE_ROUTED_PREFIX}" | cut -d'/' -f1 | sed 's/::.*$/::/')
+# This is the corrected command using a valid range for --to-source
+ip6tables -t nat -A POSTROUTING -o "${HE_TUNNEL_IF}" -j SNAT --to-source "${NETWORK_PART}2-${NETWORK_PART}ffff:ffff:ffff:ffff" --random
+echo "      iptables rule installed to randomize outgoing IPs."
 
 echo "[5/5] Starting SOCKS5 proxy server..."
 
