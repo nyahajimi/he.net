2d5849119463bc1cd477291b160c75b607d407ed
nyahajimi <7547386+hajimi@gmail.com>
2025-09-20 00:17:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,25 +12,31 @@ HE_TUNNEL_IF=${HE_TUNNEL_IF:-he-tunnel}
 echo "--- IPv6 Random Proxy Entrypoint ---"
 echo "------------------------------------"
 
+# --- THIS IS THE ROBUST FIX FOR THE RACE CONDITION ---
+# Wait patiently for the tunnel interface to be moved into this container.
+echo "[1/6] Waiting for tunnel interface '${HE_TUNNEL_IF}' to appear..."
+while [ ! -d "/sys/class/net/${HE_TUNNEL_IF}" ]; do
+  echo "      '${HE_TUNNEL_IF}' not found, sleeping for 1 second..."
+  sleep 1
+done
+echo "      Interface '${HE_TUNNEL_IF}' found!"
+
+
 # --- Network Configuration ---
-echo "[1/5] Configuring IPv6 address for tunnel interface..."
+echo "[2/6] Configuring IPv6 address for tunnel interface..."
 ip addr add "${HE_CLIENT_IPV6}" dev "${HE_TUNNEL_IF}"
 
-echo "[2/5] Bringing tunnel interface up..."
+echo "[3/6] Bringing tunnel interface up..."
 ip link set "${HE_TUNNEL_IF}" up
 
-echo "[3/5] Setting default IPv6 route..."
+echo "[4/6] Setting default IPv6 route..."
 ip route add ::/0 dev "${HE_TUNNEL_IF}"
 
-echo "[4/5] Applying random source NAT (SNAT) rule..."
-# Extract the network prefix part (e.g., 2001:470:d:def) from "2001:470:d:def::/64"
-# We use cut on ':' to get the first 4 blocks. This is robust.
+echo "[5/6] Applying random source NAT (SNAT) rule..."
 NETWORK_PREFIX=$(echo "${HE_ROUTED_PREFIX}" | cut -d':' -f1-4)
-
-# This is the corrected and robust ip6tables command.
-ip6tables -t nat -A POSTROUTING -o "${HE_TUNNEL_IF}" -j SNAT --to-source "${NETWORK_PREFIX}::2-${NETWORK_PREFIX}:ffff:ffff:ffff:ffff" --random
+ip6tables -t nat -A POST_ROUTING -o "${HE_TUNNEL_IF}" -j SNAT --to-source "${NETWORK_PREFIX}::2-${NETWORK_PREFIX}:ffff:ffff:ffff:ffff" --random
 echo "      iptables rule installed to randomize outgoing IPs."
 
-echo "[5/5] Starting SOCKS5 proxy server..."
+echo "[6/6] Starting SOCKS5 proxy server..."
 
 exec microsocks -u "${SOCKS_USER}" -p "${SOCKS_PASS}"
